b64fd9d7add51573a91c3c9c71d2e0e6140e3750
GitHub Action <action@github.com>
2022-08-31 16:02:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/nikitashchienko/jetbrainspluginsandbox/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nikitashchienko/jetbrainspluginsandbox/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikitashchienko.jetbrainspluginsandbox.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikitashchienko.jetbrainspluginsandbox.MyBundle
 
 class MyApplicationService {
 
